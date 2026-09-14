b09c27ea23880c0a3093ebe1e1097abb6c793892
hudaysawalha-web <hudasawalha3@gmail.com>
2026-09-14 23:03:11 +0300

    Add Hero component with styling for layout and design

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import Avatar from "../../assets/avatars.svg";
+import { FaStar } from "react-icons/fa";
 
+import "./Hero.css";
 export default function Hero() {
   return (
-    <div>
-      <img  src={Avatar} alt="Avatar" />
-      <h1>Start BootStrap</h1>
-
-      <p>Graphic Artist - Web Designer - Illustrator</p>
+  <div className="masthead">
+    <div className="portfolio-container">
+      <img src={Avatar} alt="Avatar" className="masthead-avatar"/>
+      <h1 className="masthead-heading">Start BootStrap</h1>
+      <div class="divider-custom divider-light">
+        <div class="divider-custom-line"></div>
+        <div class="divider-custom-icon">
+          <FaStar className="star-icon"/>
+        </div>
+        <div class="divider-custom-line"></div>
+      </div>
+      <p className="masthead-subheading">Graphic Artist - Web Designer - Illustrator</p>
+    </div>
     </div>
   );
 }
