ce09b44a93a356277cb9e695a76e661f9c519740
hudaysawalha-web <hudasawalha3@gmail.com>
2026-09-14 22:10:32 +0300

    Refactor Navbar component and styles for improved layout and functionality

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,47 @@
 import React from "react";
+import { useState } from "react";
 import "./Navbar.css";
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <>
-      <div className="navbar-container">
+      <nav className="navbar-container">
         <div className="navbar-content">
-          <a className="navbar-brand" href="#home">
-            start bootstrap
-          </a>
-          <div className="navbar-nav ">
-            <ul className="navbarResponsive " >
-              <li className="nav-item"><a className="nav-link" href="#portfolio">Portfolio</a></li>
-              <li className="nav-item"><a className="nav-link" href="#about">About</a></li>
-              <li className="nav-item"><a className="nav-link" href="#contact">Contact</a></li>
+          <div className="navbar-brand-container">
+            <a className="navbar-brand" href="#home">
+              start bootstrap
+            </a>
+
+            <button className="navbar-toggler">
+              Menu
+            </button>
+          </div>
+          <div className= "navbar-collapser ">
+            <ul className="navbar-list">
+              <li className="nav-item">
+                <a className="nav-link" href="#portfolio">
+                  Portfolio
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#about">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#contact">
+                  Contact
+                </a>
+              </li>
             </ul>
           </div>
         </div>
-      </div>
+      </nav>
     </>
   );
 }
