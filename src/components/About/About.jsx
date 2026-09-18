6d700862bf3591d9d7c766b533d102fe0b72f2b9
hudaysawalha-web <hudasawalha3@gmail.com>
2026-09-18 20:17:19 +0300

    Refactor About component layout and styles for improved design and functionality

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,36 +1,41 @@
 import React from "react";
 import downloadIcon from "../../assets/download-svgrepo-com.svg";
 import DividerCustom from "../Divider/dividerCustome";
+import { FaDownload } from "react-icons/fa";
 import { Button } from "react-bootstrap";
+import "../About/About.css";
+
 export default function About() {
   return (
-    
-      <div className="masthead">
-        <div>
-          <h2>About</h2>
+    <div className="About-section page-section">
+      <div className="container">
+        <h2>About</h2>
+
+        <DividerCustom />
+        <div className="row">
+          <div className="col-lg-4 ms-auto">
+            <p className="lead">
+              Freelancer is a free bootstrap theme created by Start Bootstrap.
+              The download includes the complete source files including HTML,
+              CSS, and JavaScript as well as optional SASS stylesheets for easy
+              customization.
+            </p>
+          </div>
+          <div className="col-lg-4 me-auto">
+            <p className="lead">
+              You can create your own custom avatar for the masthead, change the
+              icon in the dividers, and add your email address to the contact
+              form to make it fully functional!
+            </p>
+          </div>
         </div>
-        <DividerCustom/>
-        <div>
-          <p>
-            Freelancer is a free bootstrap theme created by Start Bootstrap. The
-            download includes the complete source files including HTML, CSS, and
-            JavaScript as well as optional SASS stylesheets for easy
-            customization.
-          </p>
-          <p>
-            You can create your own custom avatar for the masthead, change the
-            icon in the dividers, and add your email address to the contact form
-            to make it fully functional!
-          </p>
-        </div>
-        <div>
-          <Button>
-            <img src={downloadIcon} alt="Download" width="20" height="20" />
+        <div className="about-button">
+          <a className="btn-about" href="#downloadpage">
+            <FaDownload className="btn-about-icon" />
             Free Download!
-          </Button>
-       
+          </a>
         </div>
       </div>
-  
+    </div>
   );
 }
